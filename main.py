01fc8f9241e7a306680b78f6f80b3ae850d827fd
Eliana Blanco <114587147+ElianaBlanco@users.noreply.github.com>
2023-11-30 23:40:37 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+# Pedir el nombre del jugador por teclado
+nombre_jugador = input("Ingresa tu nombre: ")
+
+# Imprimir un mensaje de bienvenida con el nombre del jugador
+print("¡Bienvenido,", nombre_jugador + "! Disfruta del juego.")
